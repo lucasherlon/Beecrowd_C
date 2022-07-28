4966f878b9e4e35e3d7a7a56df3661207d4f173c
Lucas Herlon <62165683+lucasherlon@users.noreply.github.com>
2022-07-28 17:41:28 -0300

    2414 - Desafio do Maior Número

---
+++ maiornumero_2414.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+    int  maior, nota;
+    maior = 0;
+    while(1){
+        scanf("%d", &nota);
+        if ( nota ==0){
+            break;
+        }
+        if (nota >= maior){
+            maior = nota;
+        }
+       }
+    printf("%d\n", maior); 
+    return 0;
+}

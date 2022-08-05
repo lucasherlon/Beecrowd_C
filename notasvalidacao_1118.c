3f7ebb2a0a235c9bd548e1d02111c9b18cccb39c
Lucas Herlon <62165683+lucasherlon@users.noreply.github.com>
2022-08-05 20:01:57 -0300

    Problema: 1118 - Várias Notas Com Validação

---
+++ notasvalidacao_1118.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    
+    double nota, media, soma;
+    int i, resp=1;
+    
+    while(resp == 1){
+        soma = 0;
+        for(i=0; i<2; i++){
+            scanf("%lf", &nota);
+            while(nota<0 || nota>10){
+                printf("nota invalida\n");
+                scanf("%lf", &nota);
+            }
+        soma += nota;
+        }
+        media = soma/2;
+        printf("media = %.2lf\n", media);
+        
+        do {printf("novo calculo (1-sim 2-nao)\n");
+            scanf("%d", &resp);
+            if (resp==2){
+                break;
+            }
+       }while(resp!= 1 && resp!=2);
+    }
+    return 0;
+}

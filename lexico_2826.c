a6ae363e7c336d2eefefdf57e27022d6ae8ffcbd
Lucas Herlon <62165683+lucasherlon@users.noreply.github.com>
2022-09-07 12:52:43 -0300

    Problema: 2826 - Léxico

---
+++ lexico_2826.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char p1[21];
+    char p2[21];
+    
+    scanf("%s %s", p1, p2);
+    
+    if(strcmp(p1,p2)==0 || strcmp(p1,p2)==-1){
+        printf("%s\n", p1);
+        printf("%s\n", p2);
+    }else if(strcmp(p1,p2)==1){
+        printf("%s\n", p2);
+        printf("%s\n", p1);
+    }
+
+    return 0;
+}
